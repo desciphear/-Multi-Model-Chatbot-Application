5099828c0b242b72587c19f3399c5630f23dc5bc
Piyush <167032565+desciphear@users.noreply.github.com>
2025-03-25 16:32:29 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -6,9 +6,7 @@ import os
 st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
+    .StHeader{
         display: none;
     }
     </style>
